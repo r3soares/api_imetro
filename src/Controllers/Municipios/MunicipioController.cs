776119bede390449f8a437e0c8defe31144381be
Ricardo Martins Brasil Soares <darkrick3@msn.com>
2021-11-08 13:29:38 -0300

    Alterado caminho para acesso ao controller

--- src/Controllers/Municipios/MunicipioController.cs
+++ src/Controllers/Municipios/MunicipioController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace src.Controllers.Vtr
 {
-    [Route("api/[controller]")]
+    [Route("api/sgi/[controller]")]
     [ApiController]
     public class MunicipioController : ControllerBase
     {
